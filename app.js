8652ccb8a136bc99095d74c50be12a0e443551ce
tk315 <tusharkhandelwal315@gmail.com>
2021-11-25 03:38:17 +0530

    app.js modified

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const path=require('path')
 const app=express();
 
 const port=process.env.PORT || 1131;
-app.listen(port);
+
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'View','index.html'));
+    res.send('hello world');
+    //res.sendFile(path.join(__dirname,'View','index.html'));
 });
+
+app.listen(port);
